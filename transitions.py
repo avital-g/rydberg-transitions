e168be98f279e51106755de1a350a4ad061d17ae
Avisgi <avital.giat@mail.huji.ac.il>
2022-07-25 18:01:58 +0300

    with Shanghai data and plot

--- transitions.py
+++ transitions.py
@@ -1,17 +1,43 @@
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
 
+from Tools.scripts.ndiff import fopen
 
 "CONSTANTS"
 c = 299792458
 UPPER_GROUND_STATE_FRQ = 2.56300597908911*(10**9)
+TRANS_FROM_GROUND_STATE =  384.2304844685 * (10**12)
 
-def wl_to_frq_unit_chnage(wl):
-    return c/(wl)
+def wl_frq_unit_chnage(wl):
+    return c/wl
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def calculate_rydberg_wl_shanghai(frq):
+    rydberg_transition = frq - TRANS_FROM_GROUND_STATE
+    print (rydberg_transition)
+    return (wl_frq_unit_chnage(rydberg_transition))
 
+
+def read_Shanghai_text():
+    data = np.empty((0,2), float)
+    with open ("Shanghai_P_transitions.txt") as f:
+        for line in f:
+            n = ""
+            frq = ""
+            i = 0
+            while line [i] !=" ":
+                n = n+ line[i]
+                i=i+1;
+            i=i+1
+            while line [i] !=" ":
+                frq = frq + line[i]
+                i=i+1
+            row = [float(n), calculate_rydberg_wl_shanghai(float(frq)*m.pow(10,12))]
+            data = np.append(data, np.array([row]), axis=0)
+    return (data);
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    data = read_Shanghai_text()
+    plt.plot (data[:,0], data[:,1])
+    plt.show()
