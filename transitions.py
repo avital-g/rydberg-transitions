d50e593ff52b163c93350efdf26efcf3be02b2f2
Avisgi <avital.giat@mail.huji.ac.il>
2022-07-25 12:35:15 +0300

    first change

---
+++ transitions.py
@@ -0,0 +1,15 @@
+
+
+"CONSTANTS"
+c = 299792458
+UPPER_GROUND_STATE_FRQ = 2.56300597908911*(10**9)
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
