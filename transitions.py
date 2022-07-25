ebb4ba0b0781dcaae83a093849938a63c4e2f181
Avisgi <avital.giat@mail.huji.ac.il>
2022-07-25 13:14:10 +0300

    "try again"

--- transitions.py
+++ transitions.py
@@ -4,6 +4,8 @@
 c = 299792458
 UPPER_GROUND_STATE_FRQ = 2.56300597908911*(10**9)
 
+def wl_to_frq_unit_chnage(wl):
+    return c/(wl)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
